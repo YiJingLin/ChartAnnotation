d09d5c46af840f633043b55bd84b2e2057b2e2ea
YiJingLin <r06921076@ntu.edu.tw>
2021-05-16 17:56:42 +0800

    feat: interactive cut line and callback

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Introduction
+The main chart with range sidebar which decide cut line start/end point. The second image show we can save cut line information using callback.
+
+<img src="./imgs/main_chart.png" width="90%"/>
+
+<img src="./imgs/save_callback.png" width="90%"/>
+
 
 
 # Structure
@@ -13,5 +19,5 @@ Application entry point: wsgi.py[[2]](https://en.wikipedia.org/wiki/Web_Server_G
 
 
 # Reference
-1. [Flask web with plotly](https://hackersandslackers.com/plotly-dash-with-flask/) 
-2. [Web Server Gateway Interface (WSGI)](https://en.wikipedia.org/wiki/Web_Server_Gateway_Interface)
+1. [[Article] Flask web with plotly](https://hackersandslackers.com/plotly-dash-with-flask/) 
+2. [[Wikipedia] Web Server Gateway Interface (WSGI)](https://en.wikipedia.org/wiki/Web_Server_Gateway_Interface)
--- application/plotlydash/dashboard.py
+++ application/plotlydash/dashboard.py
@@ -2,11 +2,13 @@
 import numpy as np
 import pandas as pd
 import dash
-import dash_table
+from dash.dependencies import Input, Output, State
 import dash_html_components as html
 import dash_core_components as dcc
+import plotly.graph_objs as go
 
-from .data import create_dataframe
+
+# from .data import create_dataframe
 from .layout import html_layout
 
 
@@ -20,46 +22,70 @@ def init_dashboard(server):
             'https://fonts.googleapis.com/css?family=Lato'
         ]
     )
-
-    # Load DataFrame
-    df = create_dataframe()
-
+    
+    # example data
+    import random
+    x = np.arange(30)
+    y = [ e * 5 * random.randint(3, 10) for e in x]
+    
     # Custom HTML layout
     dash_app.index_string = html_layout
 
     # Create Layout
     dash_app.layout = html.Div(
-        children=[dcc.Graph(
-            id='histogram-graph',
-            figure={
-                'data': [{
-                    'x': df['complaint_type'],
-                    'text': df['complaint_type'],
-                    'customdata': df['key'],
-                    'name': '311 Calls by region.',
-                    'type': 'histogram'
-                }],
-                'layout': {
-                    'title': 'NYC 311 Calls category.',
-                    'height': 500,
-                    'padding': 150
-                }
-            }),
-            create_data_table(df)
+        children=[
+            dcc.Graph(
+                id='line-chart',
+                figure = go.Figure(data=[go.Scatter(x=x, y=y)])
+            ),
+            dcc.RangeSlider(
+                id = 'range-slider',
+                marks={i: 't{}'.format(i) for i in range(len(x))},
+                min=x[0],
+                max=x[-1],
+                value=[x[0], x[-1]]
+            ),
+            html.Button('Submit', id='button'),
+            html.Div(id='output-container-button',
+             children='Enter a value and press submit')
+#             dcc.Graph(
+#                 id='line2',
+#                 figure = go.Figure(data=[go.Scatter(x=x, y=x**2)])
+#             ),
+#             dcc.RangeSlider(
+#                 id = 'rs2',
+#                 marks={i: 'Label {}'.format(i) for i in range(-5, 7)},
+#                 min=-5,
+#                 max=6,
+#                 value=[-3, 4]
+#             ),
         ],
         id='dash-container'
     )
+
+    init_callbacks(dash_app, x, y)
+
     return dash_app.server
 
 
-def create_data_table(df):
-    """Create Dash datatable from Pandas DataFrame."""
-    table = dash_table.DataTable(
-        id='database-table',
-        columns=[{"name": i, "id": i} for i in df.columns],
-        data=df.to_dict('records'),
-        sort_action="native",
-        sort_mode='native',
-        page_size=300
-    )
-    return table
+def init_callbacks(dash_app, x, y):
+    @dash_app.callback(
+        Output("line-chart", "figure"), 
+        [Input("range-slider", "value")])
+    def update_line_chart(range_value):
+        start, end = range_value
+        print(start, end)
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=x, y=y))
+        fig.add_trace(go.Scatter(x=[start, end], y=[y[start], y[end]]))
+        return fig
+    
+    @dash_app.callback(
+        Output('output-container-button', 'children'),
+        [Input('button', 'n_clicks')],
+        [State('range-slider', 'value')])
+    def save_line(n_clicks, value):
+        x_start, x_end = value
+        y_start, y_end = y[x_start], y[x_end]
+#         print('start: ({}, {}), end: ({}, {})'.format(x_start, y_start, x_end, y_end))
+        return 'start point: ({}, {}), end point: ({}, {}) saved'.format(x_start, y_start, x_end, y_end)
